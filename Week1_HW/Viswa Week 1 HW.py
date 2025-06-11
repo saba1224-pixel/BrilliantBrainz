99f2611c5adc1ca5367021c81ff34a3c2790a3cd
VishiGotDrip <108681366+VishiGotDrip@users.noreply.github.com>
2025-06-10 20:11:20 -0400

    Create Viswa Week 1 HW.py

---
+++ Week1_HW/Viswa Week 1 HW.py
@@ -0,0 +1,15 @@
+print("Welcome to Python Programming!")
+age="15"
+print("The number defined is:"+ age)
+a=7
+b=5
+sum=a+b
+print(sum)
+x=4
+y=6
+print(x*y)
+x1=8
+y1=2
+print(x1/y1)
+print(x1%y1)
+print("A compiler translates an entire code into another language, when an interpreter executes and interprets the code line by line.")
