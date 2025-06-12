d98921165813b5323c9a386b8ce2a3d62137d4d3
saba1224-pixel <ms.sabasultana@gmail.com>
2025-06-12 14:00:41 -0400

    Add files via upload

---
+++ Week1_HW/Week1HWkavinsoundararajan.py
@@ -0,0 +1,17 @@
+print("Welcome to Python programming!")
+age="14"
+print("The Number defined is: "+ age)
+a=7
+b=5
+print(a+b)
+x=14
+y=6
+print(x*y)
+x=8
+y=2
+print(x/y)
+x=8 
+y=2
+print(x%y)
+
+print("The difference between an interpreter and a compiler is that an interpreter executes the code plainly. A compiler translate the code to other languages.")
